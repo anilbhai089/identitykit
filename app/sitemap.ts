beeafc0b5fe3204ff4a03aee13baf98637eb51dc
anilbhai089 <prajapatianil9735@gmail.com>
2026-06-19 17:37:55 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -131,5 +131,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+
+    {
+      url: `${baseUrl}/web-stories/asci-disclosure-rules.html`,
+      lastModified: today,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }

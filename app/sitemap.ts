40f3f53e39ec96234670e9d00610b9cd3feaf51e
anilbhai089 <prajapatianil9735@gmail.com>
2026-06-19 22:35:21 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -138,5 +138,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+
+     {
+      url: `${baseUrl}/web-stories/influencer-payment-terms.html`,
+      lastModified: today,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }

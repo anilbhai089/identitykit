de0fc06af7304b07c1f61acde95d5d101b1ee15c
anilbhai089 <prajapatianil9735@gmail.com>
2026-06-19 09:27:56 +0530

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/dashboard', '/onboarding'],
+      },
+    ],
+    sitemap: 'https://identitykit.in/sitemap.xml',
+  }
+}

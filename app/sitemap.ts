67b570bff0ad9a2558ef517a4307408803f23d90
anilbhai089 <prajapatianil9735@gmail.com>
2026-06-19 13:24:28 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -103,5 +103,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+
+     {
+      url: `${baseUrl}/web-stories/engagement-rate-formula.html`,
+      lastModified: today,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }

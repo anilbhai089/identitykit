03fbc1e0c24e3929ee1fd03bb21be4e32e22e758
anilbhai089 <prajapatianil9735@gmail.com>
2026-08-11 11:37:34 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -756,6 +756,20 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+
+       {
+      url: `${baseUrl}/blog/threads-monetization-2026`,
+      lastModified: today,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+
+       {
+      url: `${baseUrl}/blog/youtube-shorts-monetization-2026`,
+      lastModified: today,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
     
 
        
